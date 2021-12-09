2449815dd1463fbfffe7dc2695f904c73fdf9918
Sid <git@najjah.dev>
2021-12-09 08:40:22 -0300

    Ansible installer

---
+++ install_ansible.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo -v &&
+  sudo apt update &&
+  sudo apt install -y software-properties-common &&
+  sudo add-apt-repository --yes --update ppa:ansible/ansible &&
+  sudo apt install -y --autoremove ansible
